1014c3b300970033b7ebc7640f12691c765936ca
tanilserbes <serbestanil@gmail.com>
2020-05-11 17:37:31 +0100

    5 returns Buzz

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -2,8 +2,12 @@ describe('FizzBuzz', function() {
   it('1 returns 1', function() {
     expect(fizzBuzz(1)).toEqual(1);
   });
-  
+
   it('3 returns Fizz', function() {
     expect(fizzBuzz(3)).toEqual('Fizz')
+  });
+  
+  it('5 returns Buzz', function() {
+    expect(fizzBuzz(5)).toEqual('Buzz')
   });
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,6 +1,9 @@
 function fizzBuzz(n) {
   if (n === 3) {
     return 'Fizz';
-  } 
+
+  } else if (n === 5) {
+    return 'Buzz';
+  }
     return n;
 }
